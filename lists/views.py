b8b4ae830f31a9b45a94072d37eb28eb29f0a982
John Ryan <38575933+jhryan@users.noreply.github.com>
2020-01-29 06:57:43 +0000

    Create new List model

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
 from lists.models import Item
+from lists.models import List
 
 
 def home_page(request):
@@ -8,7 +9,8 @@ def home_page(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 
