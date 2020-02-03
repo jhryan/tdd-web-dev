ee50fcfa41ea2a5c8c2745b9e11cc0f5d6b54d29
John Ryan <38575933+jhryan@users.noreply.github.com>
2020-02-03 07:29:13 +0000

    Add form for adding items to existing lists

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -95,6 +95,21 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+    from unittest import skip
+    @skip('not ready')
+    def test_duplicate_item_validation_errrors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
