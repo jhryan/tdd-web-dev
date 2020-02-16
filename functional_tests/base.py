61bfd4011e8f7378869aca1e6141cc624f3bc7a4
John Ryan <38575933+jhryan@users.noreply.github.com>
2020-02-16 07:21:42 +0000

    --amend

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -28,9 +28,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         options.set_headless()
         self.browser = webdriver.Chrome(options=options)
 
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
 
     def tearDown(self):
         self.browser.quit()
