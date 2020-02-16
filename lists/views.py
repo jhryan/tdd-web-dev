1291a73bfdebd9373435d8014e5fd3a9b039b218
John Ryan <38575933+jhryan@users.noreply.github.com>
2020-02-16 23:54:13 +0000

    Add list owners via forms with more isolated tests

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from lists.forms import ExistingListItemForm
 from lists.forms import ItemForm
+from lists.forms import NewListForm
 from lists.models import List
 
 User = get_user_model()
@@ -14,15 +15,11 @@ def home_page(request):
 
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
 
 
 def view_list(request, list_id):
